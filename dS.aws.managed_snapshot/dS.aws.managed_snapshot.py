ce886de660a60450f5f31959f04cc3ccada3ad44
Deca Sight <deca.sight@gmail.com>
2018-03-25 21:51:01 -0300

    Show all snapshots in list if --all is given

--- dS.aws.managed_snapshot/dS.aws.managed_snapshot.py
+++ dS.aws.managed_snapshot/dS.aws.managed_snapshot.py
@@ -26,7 +26,8 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only Snapshots for project(tag Project:<name>)")
-def list_volumes(project):
+@click.option("--all", 'list_all', default=False, is_flag=True, help="List all snapshots for each volume, not just the most recent")
+def list_snapshots(project):
     """ List EC2 snapshots """
 
     instances = filter_instances(project)
@@ -41,6 +42,7 @@ def list_volumes(project):
                         s.progress,
                         s.start_time.strftime("%c")
                     )))
+                if s.state == 'completed' and not list_all: break 
     return
 
 @cli.group('volumes')
